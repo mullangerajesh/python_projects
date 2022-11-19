5c4e4570b44d0c3243a0b7d8f3cd23f51419ee22
mullangerajesh <mullangerajesh@gmail.com>
2022-11-19 14:41:16 +0530

    This is Calculator Program file

---
+++ calculator.py
@@ -0,0 +1,26 @@
+n1=int(input("enter 1st number: "))
+n2=int(input("enter 2nd number: "))
+opt=input("enter a operator :")
+def add():   
+   if opt==("+") :
+      print(n1+n2)
+   
+def sub():
+   if opt==("-") :
+      print(n1-n2)
+  
+def mul():
+   if opt==("*"):
+      print(n1*n2)
+                      
+def div():
+   if opt==("/") :
+      print(n1/n2)
+   
+add()
+sub()
+mul()
+div()
+   
+
+
---
+++ vowels.py
@@ -0,0 +1,7 @@
+#write a programme to find given user letter is vowel or not ?
+vowels="aeiou"
+user_letters=input("enter any letter :  ")
+if user_letters  in vowels:
+    print(f"{user_letters} is vowel")
+else:
+    print(f"sorryyy ....... _{user_letters} _  is not vowel")
