7a5f1be82682744a92fe6e6b136769973c709f47
mullangerajesh <mullangerajesh@gmail.com>
2022-11-20 15:40:33 +0530

    sunday evening 1st commiting

---
+++ swap.py
@@ -0,0 +1,9 @@
+x=5
+y=6
+#creating a temporary variable
+temp=x
+x=y
+y=temp
+#x,y values are swapped
+print("value of x after Swapping is :{}".format(x))
+print("value of y after swapping is :{}".format(y))
